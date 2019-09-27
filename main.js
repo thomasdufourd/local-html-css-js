6cb249550246da71b4133cdd5173df85cc41feef
Thomas Dufourd <thomas.dufourd@gmail.com>
2019-09-27 22:54:07 +0200

    added some changes to demonstrate DOM manipulation

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ window.onload = addElement;
 function addElement() {
   // create a new div element and give it some content 
   var newDiv = document.createElement("div"); 
-  var newContent = document.createTextNode("Hi there and greetings!");
+  var newContent = document.createTextNode("Hi there ***** greetings!");
   // add the text node to the newly created div.
   newDiv.appendChild(newContent);
   // add the newly created element and its content into the DOM 
